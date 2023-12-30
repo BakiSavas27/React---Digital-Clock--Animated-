478c75248f74664a7893fa9ba8a5061ffec003fe
BakiSavas27 <savasbaki25@gmail.com>
2023-12-30 14:11:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import 'animate.css';
+
+function App() {
+  let time = new Date().toLocaleTimeString()
+  const [currentTime, setcurrentTime] = useState(time)
+
+  function Time(){
+    let time = new Date().toLocaleTimeString()
+    setcurrentTime(time)
+  }
+
+  setInterval(Time,1000)
+
+  return (
+    <div className='App'>
+      <h1 class="animate__animated animate__zoomInDown">{currentTime}</h1>
+    </div>
+  )
+}
+
+export default App
